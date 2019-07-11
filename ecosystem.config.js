4ca5fe1cd5a1337b3a0ccb813e7dc86427f12f70
Uice Lu <uicestone@gmail.com>
2019-07-11 08:48:53 +0800

    disable pm2 watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
   apps: {
     name: "kangazone-server",
     script: "./dist/index.js",
-    watch: ["./dist"],
     log_date_format: "YYYY-MM-DD HH:mm:ss.SSS (ZZ)"
   },
   deploy: {
