da1847dd640783d15769d9df97347cf2c11d92d1
Uice Lu <uicestone@gmail.com>
2019-09-20 02:39:23 +0800

    use ts-node in pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apps: {
     name: "kangazone-server",
-    script: "./dist/index.js",
+    script: "./node_modules/.bin/ts-node",
+    args: "src/index.ts",
     log_date_format: "YYYY-MM-DD HH:mm:ss.SSS (ZZ)"
   },
   deploy: {
