607597827c019231422132b5e1aaad273878cb64
Uice Lu <uicestone@gmail.com>
2019-09-23 18:08:11 +0800

    fix save array field creates new id and lost ref

--- src/middlewares/castEmbedded.ts
+++ src/middlewares/castEmbedded.ts
@@ -2,10 +2,13 @@ export default async function(req, res, next) {
   for (let key in req.body) {
     if (
       req.body[key] instanceof Object &&
-      ["code", "payment", "store", "user", "customer"].includes(key) &&
+      ["code", "store", "user", "customer"].includes(key) &&
       req.body[key].id
     ) {
       req.body[key] = req.body[key].id;
+    }
+    if (["payments", "codes"].includes(key) && Array.isArray(req.body[key])) {
+      req.body[key] = req.body[key].map(item => item.id || item);
     }
   }
   next();
