f23b5a1944eb2ca114459bd424ca5808e6e36a79
Uice Lu <uicestone@gmail.com>
2019-10-18 08:06:09 +0800

    more stats for daily reconciliation

--- src/apis/StatsRouter.ts
+++ src/apis/StatsRouter.ts
@@ -1,16 +1,36 @@
 import handleAsyncErrors from "../utils/handleAsyncErrors";
-import Booking, { BookingStatuses } from "../models/Booking";
+import Booking, {
+  BookingStatuses,
+  paidBookingStatuses
+} from "../models/Booking";
 import moment from "moment";
+import { config } from "../models/Config";
+import Payment, { Gateways } from "../models/Payment";
 
 export default router => {
   // Store CURD
   router
-    .route("/stats")
+    .route("/stats/:date?")
 
     .get(
       handleAsyncErrors(async (req, res) => {
-        const today = moment().format("YYYY-MM-DD");
-        const bookingsToday = await Booking.find({ date: today });
+        const dateInput = req.params.date;
+        const dateStr = moment(dateInput).format("YYYY-MM-DD"),
+          startDate = moment(dateInput).startOf("day"),
+          endDate = moment(dateInput).endOf("day");
+
+        const coupons = config.coupons;
+
+        const bookingsPaid = await Booking.find({
+          date: dateStr,
+          status: { $in: paidBookingStatuses }
+        });
+        const payments = await Payment.find({
+          createdAt: {
+            $gte: startDate,
+            $lte: endDate
+          }
+        });
         const bookingServing = await Booking.find({
           status: BookingStatuses.IN_SERVICE
         });
@@ -30,12 +50,12 @@ export default router => {
           0
         );
 
-        const todayCount = bookingsToday.reduce(
+        const customerCount = bookingsPaid.reduce(
           (count, booking) => count + booking.membersCount,
           0
         );
 
-        const todayAmount = bookingsToday.reduce((amount, booking) => {
+        const paidAmount = bookingsPaid.reduce((amount, booking) => {
           return (
             amount +
             booking.payments
@@ -44,11 +64,46 @@ export default router => {
           );
         }, 0);
 
+        const socksCount = bookingsPaid.reduce(
+          (socks, booking) => socks + booking.socksCount,
+          0
+        );
+
+        const paidAmountByGateways = payments
+          .filter(p => p.paid)
+          .reduce((amountByGateways, payment) => {
+            if (!amountByGateways[payment.gateway]) {
+              amountByGateways[payment.gateway] = 0;
+            }
+            amountByGateways[payment.gateway] += payment.amount;
+            return amountByGateways;
+          }, {});
+
+        const couponsCount = bookingsPaid
+          .filter(b => b.coupon)
+          .reduce((couponsCount, booking) => {
+            let couponCount = couponsCount.find(c => c.slug === booking.coupon);
+            if (!couponCount) {
+              const coupon = coupons.find(c => c.slug === booking.coupon);
+              couponCount = {
+                slug: coupon.slug,
+                name: coupon.name,
+                count: 0
+              };
+              couponsCount.push(couponCount);
+            }
+            couponCount.count++;
+            return couponsCount;
+          }, []);
+
         res.json({
           checkedInCount,
           dueCount,
-          todayCount,
-          todayAmount
+          customerCount,
+          paidAmount,
+          socksCount,
+          paidAmountByGateways,
+          couponsCount
         });
       })
     );
