82b8d97bc0a6c409613db00e3dc8d097ffb4bc02
Uice Lu <uicestone@gmail.com>
2019-10-26 15:19:10 +0800

    fix: reset auth job runs 60 times

--- src/utils/agenda.ts
+++ src/utils/agenda.ts
@@ -147,7 +147,7 @@ agenda.on("ready", () => {
   // agenda.every("1 day", "cancel expired booked bookings");
   agenda.every("5 minutes", "finish overtime served bookings");
   // agenda.now("generate 8 digit card no");
-  agenda.every("* 1 * * *", "reset auth");
+  agenda.every("0 1 * * *", "reset auth");
 });
 
 export default agenda;
