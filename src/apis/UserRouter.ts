2cab198a6c016f1b230acffc79c1d25215dac2c5
Uice Lu <uicestone@gmail.com>
2019-11-10 11:50:11 +0800

    fix $and

--- src/apis/UserRouter.ts
+++ src/apis/UserRouter.ts
@@ -78,7 +78,7 @@ export default router => {
           });
         }
 
-        query.find($and);
+        query.find({ $and });
 
         let total = await query.countDocuments();
         const [{ totalCredit } = { totalCredit: 0 }] = await User.aggregate([
